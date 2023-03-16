59f06a17f6e2ff5f9bb1806b6f10cd30b4a862f4
martiquet <francoismartiquet@gmail.com>
2023-03-16 13:54:10 +0100

    v1.8 fix responsive / prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-import "./style/main.scss"
-import "./style/normalize.css"
+import "./style/main.scss";
+import "./style/normalize.css";
 
-import Home from './pages/Home'
-import About from './pages/About'
-import Product from './pages/Product'
-import Notfound from './pages/Notfound'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Notfound from "./pages/Notfound";
 
-import Layout from './components/Layout'
+import Layout from "./components/Layout";
 
-function App () {
-    return (
-                <Router>
-                    <Layout>
-                    <Routes>
-                        <Route path="*" element={<Notfound />} />
-                        <Route index element={<Home />} />
-                        <Route path="/About" element={<About />} />
-                        <Route path="/product/:productId" element={<Product />} />
-                    </Routes>
-                    </Layout>
-                </Router>
-    )
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="*" element={<Notfound />} />
+          <Route index element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/product/:productId" element={<Product />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import App from "./App";
-import ReactDOM from "react-dom/client"
-import reportWebVitals from './reportWebVitals';
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container)
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
 
- 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
